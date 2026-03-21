440606b87c80f89b476f7e1aeb948695bd53c3c0
김승현 <gimseunghyeon@gimseunghyeon-ui-MacBookAir.local>
2026-03-21 16:22:12 +0900

    fix sitemap format

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://maemaelog.com",
+      url: "https://maemaelog.com/",
       lastModified: new Date(),
     },
     {
